6ff72795c192b0b34f4f00a34915a39073e8eba7
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2015-04-13 15:15:41 +0200

    requete XMLHttpRequest

--- maitre/utilisateur/routes/nouveau_chantier.js
+++ maitre/utilisateur/routes/nouveau_chantier.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('OK')	
+
   res.render('nouveau_chantier', { title: 'Nouveau Chantier' });
 });
 
