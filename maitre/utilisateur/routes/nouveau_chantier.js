212c95c60279875441a243c61f3eef01d3e68987
cwamgis <cwamgis@hotmail.com>
2015-04-13 16:27:56 +0200

    envoi notification metier

--- maitre/utilisateur/routes/nouveau_chantier.js
+++ maitre/utilisateur/routes/nouveau_chantier.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+// Nécessaire pour notifier le module métier
+var messenger = require('messenger');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('OK')	
 
   res.render('nouveau_chantier', { title: 'Nouveau Chantier' });
 });
+
+// Notifie le module métier qu'il y a du boulot
+client = messenger.createSpeaker(9205);
+client.request('nouveaux chantiers a traiter', {boulot:"oui"}, function(data){
+    console.log(data);
+  });
+// Fin notification
 
 module.exports = router;
