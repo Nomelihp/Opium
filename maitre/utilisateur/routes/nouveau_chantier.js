3044db8814e7ba384829bdb9c9bcd5a1dcb6e044
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2015-04-13 16:24:17 +0200

    ajout de config.json

--- maitre/utilisateur/routes/nouveau_chantier.js
+++ maitre/utilisateur/routes/nouveau_chantier.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('OK')	
+  console.log('OK');
+  console.log(req.query.test);	
 
   res.render('nouveau_chantier', { title: 'Nouveau Chantier' });
 });
