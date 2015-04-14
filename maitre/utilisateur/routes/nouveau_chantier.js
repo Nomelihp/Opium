fb8eb6cfef09da15ebdbcf3e3a5fb7a577a66812
Nomelihp <philemon.renaud@ensg.eu>
2015-04-14 17:23:16 +0200

    developpement requete GET

--- maitre/utilisateur/routes/nouveau_chantier.js
+++ maitre/utilisateur/routes/nouveau_chantier.js
@@ -3,9 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	
+	var chantier = req.query.chantier;
+	console.log(chantier);
+	var commentaire = req.query.commentaire;
+	console.log(commentaire);
+	var optionstatue = req.query.optionstatue;
+	console.log(optionstatue);
+	
   console.log('OK')	
-
   res.render('nouveau_chantier', { title: 'Nouveau Chantier' });
 });
+
+//router.post('/', function(req, res, next) {
+//	console.log('POST');
+//	res.render('nouveau_chantier', { title: 'Nouveau Chantier' });
+//}
 
 module.exports = router;
