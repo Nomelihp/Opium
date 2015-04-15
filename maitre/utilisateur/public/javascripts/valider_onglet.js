a6e94519cd1d60c7cfd4e2d2bbf4e084c4d0c4d9
cwamgis <cwamgis@hotmail.com>
2015-04-15 09:40:15 +0200

    pb dump js phil

--- maitre/utilisateur/public/javascripts/valider_onglet.js
+++ maitre/utilisateur/public/javascripts/valider_onglet.js
@@ -65,9 +65,9 @@ function valider_onglet(id) {
     req.onreadystatechange = function (aEvt) {
       if (req.readyState == 4) {
          if(req.status == 200)
-          dump(req.responseText);
+          ;//dump(req.responseText);
          else
-          dump("Erreur pendant le chargement de la page.\n");
+          ;//dump("Erreur pendant le chargement de la page.\n");
       }
     };
     req.send();
