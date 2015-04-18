852ec3bd7f2cca35d66391c4d6cbb469ad090aeb
cwamgis <cwamgis@hotmail.com>
2015-04-18 15:33:00 +0200

    node utilisateur : maj besoins avec nom img

--- maitre/utilisateur/routes/nouveau_chantier_upload_img.js
+++ maitre/utilisateur/routes/nouveau_chantier_upload_img.js
@@ -1,8 +1,10 @@
 // Route permettant l'upload des images
 // A faire : 
+// génération exif
 // maj bd
-// maj cote client
-// envoi exif
+// creation du tableau image quand pas encore dimage
+// renvoi exif
+
 var express = require('express');
 var router  = express.Router();
 var params 	= require('../../config.json');
@@ -12,6 +14,46 @@ var Besoins = modele.besoins;
 var express = require('express');
 var multer  = require('multer');
 var fs 		= require('fs');	
+
+
+var apresUpload = function (file,req,res) {
+  // On déplace le fichier dans le répertoire de l'utilisateur et du chantier correspondants
+  
+  // Récupération du nom de l'utilisateur à partir de l'identifiant du chantier
+  Besoins.findById(req.body._id, function(err, b) {
+	  if (err) throw err;
+	
+		var bes = new Besoins(b);
+		var dir1 = params.repertoire_donnees+bes.login+"/"
+		var dir2 = dir1+req.body._id+"/";
+		
+		// Creation des repertoires correspondant au chantier si ils n'existent pas
+		if (!fs.existsSync(dir1)){
+			fs.mkdirSync(dir1);
+		}
+		if (!fs.existsSync(dir2)){
+			fs.mkdirSync(dir2);
+		}
+		// Copie du fichier
+		fs.rename(params.repertoire_donnees+"tmp/"+file.name, dir2+file.originalname, function (err) {
+		  if (err) throw err;
+		  console.log('renamed complete');
+		});
+		// Récupération des métadonnées exif de l'image
+		
+		// Update de la collection besoins avec la nouvelle image et ses metadonnees exif
+		var listeImg = [];
+		if(bes.liste_images)// il y a déjà des images dans le document
+		{
+			listeImg = bes.liste_images;
+		}
+		listeImg.push({"nom":file.originalname});
+		Besoins.findByIdAndUpdate(req.body._id, { liste_images: listeImg }, function(err, b) {
+		  if (err) throw err;
+		});
+	});
+  done=true;
+}
 
 // Paramétrage de l'upload via multer
 router.use(multer({ dest: params.repertoire_donnees+"tmp/",
@@ -23,46 +65,7 @@ router.use(multer({ dest: params.repertoire_donnees+"tmp/",
 	onFileUploadStart: function (file) {
 	  console.log(file.originalname + ' is starting ...')
 	},
-	onFileUploadComplete: function (file,req,res) {
-	  // On déplace le fichier dans le répertoire de l'utilisateur et du chantier correspondants
-	  
-	  // Récupération du nom de l'utilisateur à partir de l'identifiant du chantier
-	  Besoins.findById(req.body._id, function(err, b) {
-		  if (err) throw err;
-		
-			var bes = new Besoins(b);
-			var dir1 = params.repertoire_donnees+bes.login+"/"
-			var dir2 = dir1+req.body._id+"/";
-			
-			// Creation des repertoires correspondant au chantier si ils n'existent pas
-			if (!fs.existsSync(dir1)){
-				fs.mkdirSync(dir1);
-			}
-			if (!fs.existsSync(dir2)){
-				fs.mkdirSync(dir2);
-			}
-			// Copie du fichier
-			fs.rename(params.repertoire_donnees+"tmp/"+file.name, dir2+file.originalname, function (err) {
-			  if (err) throw err;
-			  console.log('renamed complete');
-			});
-			// Update de la collection besoins
-			
-		});
-	 	  
-	  done=true;
-	}
-	
-		  /*
-	  var promise = modele.besoins.find({ _id: req.body._id }).exec();
-		 promise.then(function(besoin){
-			var bes = new Besoins(besoin[0]);
-			console.log("yououh je suis la : "+bes.commentaire);
-			
-			
-		});
-	  */
-	
+	onFileUploadComplete: apresUpload	
 }));
 
 router.get('/', function(req, res){
