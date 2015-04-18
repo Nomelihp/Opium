972819ce682ac040d71e63eec1f2242e1df83ee8
cdrouadaine <clement.drouadaine@orange.fr>
2015-04-18 18:35:06 +0200

    Progression dans la récupération des données de paramétrage du chantier

--- maitre/utilisateur/public/javascripts/getParameters.js
+++ maitre/utilisateur/public/javascripts/getParameters.js
@@ -5,23 +5,19 @@ var myTable = "";
 var whatever = function onPageOpen() {
     var myResume = document.getElementById("resumeParam");
     
-    //liste des paramètres obligatoires
-    var importants = ['date','nom','etat','type','liste_images'];
-    
-    //liste des paramètres facultatifs
-    var facultatifs = ['commentaires','masque','quantite points liaison','mise a echelle','basculement','etalonnage'];
-    
+    //ajout nom
     var nom = besoins.nom;
     if(nom != null) {
         myTable.ajout("Nom",nom);
     } else {
         myTable.ajout("Nom","NON DÉFINI","red");
     }
-    
+
+    //ajout date
     myTable.ajout("Projet créé le",besoins.date); //À CONVERTIR EN QUELQUE CHOSE DE LISIBLE
-        
-    var etat = besoins.etat;
-    switch(etat) {
+
+    //ajout etat du calcul
+    switch(besoins.etat) {
         case 1:
             myTable.ajout("État","En attente de l'utilisateur","blue");
             break;
@@ -35,27 +31,75 @@ var whatever = function onPageOpen() {
             myTable.ajout("État","Calcul terminé avec succès","green");
             break;
         case 5:
-            myTable.ajout("État","Échec","red");
+            myTable.ajout("État","ÉCHEC","red");
             break;
     }
 
+    //ajout commentaires
     var commentaires = besoins.commentaires;
     if(commentaires != null) {
         myTable.ajout("Commentaires",commentaires);
     }
-    
+
+    //ajout type (Statue/Facade)
     var type = besoins.type;
-    if(nom != null) {
+    if(type != null) {
         myTable.ajout("Type",type);
     } else {
         myTable.ajout("Type","NON DÉFINI","red");
     }
-    
+
+    //ajout liste images, sous forme de menu déroulant
     var liste_images = besoins.liste_images;
-    if(nom != null) {
-        myTable.ajout("Type",liste_images);
+    var menuDeroulant = ""; //variable où stocker le menu en attendant de le mettre dans myTable
+    if(liste_images != null) {
+        menuDeroulant += "<select size='3'>\n";
+        //transformation du string en array
+        liste_images = liste_images.replace("[","").replace("]","").split(",");
+
+        for(var i=0; i<liste_images.length; i++) {
+            menuDeroulant += "<option>"+liste_images[i]+"</option>\n";
+        }
+        
+        menuDeroulant += "</select>"
+        myTable.ajout("Images",menuDeroulant);
     } else {
-        myTable.ajout("Type","NON DÉFINI","red");
+        myTable.ajout("Images","PAS D'IMAGES IMPORTÉES","red");
+    }
+
+    //ajout masque (2D/3D). Si pas encore défini, 3D par défaut.
+    if(besoins.masque=="22D") {
+        myTable.ajout("Masque","deux masques 2D");
+    } else {
+        myTable.ajout("Masque","un masque 3D");
+    }
+
+    //ajout nombre points liaison
+    switch(besoins.nombre_points_liaison) {
+        case 1:
+            myTable.ajout("Nombre de points de liaison","Faible");
+            break;
+        case 2:
+            myTable.ajout("Nombre de points de liaison","Moyen");
+            break;
+        case 3:
+            myTable.ajout("Nombre de points de liaison","Important");
+            break;
+        case 4:
+            myTable.ajout("Nombre de points de liaison","Maximal");
+            break;
+    }
+
+    if(besoins.mise_a_echelle == 1) {
+        myTable.ajout("Mise à l'échelle","oui");
+    } else {
+        myTable.ajout("Mise à l'échelle","non");
+    }
+
+    if(besoins.basculement == 1) {
+        myTable.ajout("Basculement","oui");
+    } else {
+        myTable.ajout("Basculement","non");
     }
 
 }
@@ -69,5 +113,5 @@ function ajout(parametre, valeur, couleur) {
         style = "style='color: "+couleur+"'"
     }
     
-    myTable += "<tr>\n\t<td>"+parametre+"</td>\n\t<td"+style+">"+valeur+"</td>\n</tr>";
+    myTable += "<tr>\n\t<td>"+parametre+"</td>\n\t<td"+style+">"+valeur+"</td>\n</tr>\n";
 }
