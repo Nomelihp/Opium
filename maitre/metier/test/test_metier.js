b0594d412418fcf609be87720e399e02865e0901
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2015-04-19 11:43:43 +0200

    préparation d'un JSON type pour le test

--- maitre/metier/test/test_metier.js
+++ maitre/metier/test/test_metier.js
@@ -1,4 +1,4 @@
-var noyau_metier = require('../local_modules');
+var noyau_metier = require('../local_modules/noyau_metier');
 var jsonTest = require('./jsonTest');
 
 noyau_metier.besoin2jobs(jsonTest);
