e99254f445666aa84e964c710ec722f9c8bab984
cwamgis <cwamgis@hotmail.com>
2015-04-20 16:57:23 +0200

    Mes chantiers : ajout liste chantiers

--- maitre/utilisateur/routes/chantiers.js
+++ maitre/utilisateur/routes/chantiers.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var params = require('../../config.json');
+
+var modele  = require('../../model/mongo_config');
+var Besoins = modele.besoins;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('chantiers', { title: 'Mes Chantiers' });
+	// Récupération des chantiers de l'utilisateur
+	Besoins.find({ login: params.login }, function(err, besoins) {
+		if (err) throw err;
+		
+		// On cree le tableau de besoins pour la vue
+		besoinsVue = [];
+		for (var i=0;i<besoins.length;i++)besoinsVue.push(new Besoins(besoins[i]));
+		
+		res.render('chantiers', { title: 'Mes Chantiers', chantiers:besoinsVue});
+	  
+	});
+	
 });
 
 module.exports = router;
