cd5774c9f083fb92ee00ff3e05ff943621a8d597
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2015-04-21 00:16:03 +0200

    init esclavee

---
+++ esclave/local_modules/comportements_esclave.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var config_esclave = require('../config_esclave')
+exports.inscription = function(){
+  var ip = config_esclave.maitre_ip
+  var port = 
+  var req = http.req
+
+}
