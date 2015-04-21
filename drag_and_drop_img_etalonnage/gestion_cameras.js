379151ec4c85b95997f10d5e775cb776d1c17d1b
cwamgis <cwamgis@hotmail.com>
2015-04-21 11:20:49 +0200

    drag and drop gestion des etalonnages

---
+++ drag_and_drop_img_etalonnage/gestion_cameras.js
@@ -0,0 +1,26 @@
+
+var nbEtalonnage = 0;
+
+// Callback pour le drag image
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+// Callback pour le drop image
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+var gestionCameras = function(fichier) {
+	var dragZone 	  = document.getElementById('drag-zone-etalonnage');
+	var nbEtalonnages = document.getElementById('nbEtalonnages');
+	
+	var drag-zone-etalonnage1 = document.getElementById('nbEtalonnages');
+	
+	nbEtalonnages.onchange = function()
+	{
+			alert("coucou");
+	};
+}(jQuery);
