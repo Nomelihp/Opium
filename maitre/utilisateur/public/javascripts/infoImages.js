2894566a3abec841081d3483c310ae1510b3bb2a
Nomelihp <philemon.renaud@ensg.eu>
2015-04-21 15:39:31 +0200

    Recuperer liste image calibration

--- maitre/utilisateur/public/javascripts/infoImages.js
+++ maitre/utilisateur/public/javascripts/infoImages.js
@@ -1,27 +1,36 @@
 
+
 //liste des infos à extraire du exif
 var myExifKeys = ["create date","file size","image size","camera model name","focal length","f number","iso","shutter speed","aperture","exposure time","sharpness","saturation","contrast","white balance","color space","orientation"];
 
 function launchAll() {
-    var myExif = JSON.parse(besoins.liste_images); //PAS LE BON PATH VERS EXIFS.
+	var idChantier = $("#idChantier").val();
+	infosChantier(idChantier,function(req){
+		var mesBesoins = JSON.parse(req.responseText);
+        // Pour mettre à jour l'interface...
+		console.log(mesBesoins);
+		nomsImages(mesBesoins);
+		//nomsBalises(mesBesoins);
+	});
     
-    nomsImages(myExif);
-    nomsBalises(myExif);    
+   
 }
 
-function nomsImages(myExif) {
-    
+function nomsImages(besoins) {
+    console.log("nomsImages");
     var myListe = "";
-    for(var i=0; i<myExif.length; i++){
-        myListe += "<option value='"+i+"'>"+myExif[i]["nom"]+"</option>";
+	var liste_images = besoins.liste_images;
+    for(var i=0; i<liste_images.length; i++){
+        myListe += "<option value='"+liste_images[i].nom+"'>"+liste_images[i].nom+"</option>";
     }
     
     document.getElementById("nomsImages").innerHTML = myListe;
-    document.getElementById("menuDeroulantImages").innerHTML = myListe;
+    document.getElementById("menuDeroulantImages").innerHTML = '<select id="menuSelectImages" class="form-control" size="3" multiple >'+myListe+'</select>';
     
 }
 
-function nomsBalises(myExif) {
+function nomsBalises(besoins) {
+	console.log("nomsBalises");
     var myListe = "";
     for(var i=0; i<myExifKeys.length; i++){
         myListe += "<option value='"+myExifKeys[i]+"'>"+myExifKeys[i]+"</option>";
@@ -31,19 +40,31 @@ function nomsBalises(myExif) {
 }
 
 function infosImage(imagePosition) {
+    console.log("infosImage");
+	var myListe = "";
+	var idChantier = $("#idChantier").val();
+	infosChantier(idChantier,function(req){
+		var mesBesoins = JSON.parse(req.responseText);
+        //Pour mettre à jour l'interface...
+		var liste_images = mesBesoins.liste_images;
+		console.log(liste_images);
+		//On cherche l'exif de l'image selectionnée
+		for(var i=0; i<liste_images.length; i++){
+			if(liste_images[i].nom == imagePosition){
+				var myImage = liste_images[i]["exif"];
+				for(var i=0; i<myExifKeys.length; i++){
+					myListe += "<tr><td>"+myExifKeys[i]+"</td><td>"+myImage[myExifKeys[i]]+"</td></tr>";
+				}
+			}
+		}
+    console.log(myListe);
+    document.getElementById("infosImage").innerHTML = myListe;
+	});
     
-    var myExif = JSON.parse(besoins.liste_images); //PAS LE BON PATH VERS EXIFS.
-    var myImage = myExif[i]["exif"];
-    
-    for(var i=0; i<myExifKeys.length; i++){
-        myListe += "<tr><td>"+myExifKeys[i]+"</td><td>"+myImage[myExifKeys[i]]+"</td></tr>";
-    }
-    
-    document.getElementById("infoImages").innerHTML = myListe;
 }
 
 function infosBalise(baliseName) {
-    
+    console.log("infosBalise");
     var myExif = JSON.parse(besoins.liste_images); //PAS LE BON PATH VERS EXIFS.
     
     for(var i=0; i<myExif.length; i++){
