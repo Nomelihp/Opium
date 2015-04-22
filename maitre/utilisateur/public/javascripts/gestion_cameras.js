57628aa434de3c5fac8791b607f5a9f15c35162b
Nomelihp <philemon.renaud@ensg.eu>
2015-04-22 12:05:31 +0200

    ajout de fichiers pour demo

--- maitre/utilisateur/public/javascripts/gestion_cameras.js
+++ maitre/utilisateur/public/javascripts/gestion_cameras.js
@@ -1,5 +1,27 @@
-// Ajoute une image dans la zone de sélection des images de l'étalonnage à partir d'un nom de fichier
-function ajouteImageEtalonnage(nomFichier)
-{
-	
+
+var nbEtalonnage = 0;
+
+// Callback pour le drag image
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
+
+// Callback pour le drop image
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+var gestionCameras = function(fichier) {
+	var dragZone 	  = document.getElementById('drag-zone-etalonnage');
+	var nbEtalonnages = document.getElementById('nbEtalonnages');
+	
+	var drag-zone-etalonnage1 = document.getElementById('nbEtalonnages');
+	
+	nbEtalonnages.onchange = function()
+	{
+			alert("coucou");
+	};
+}(jQuery);
