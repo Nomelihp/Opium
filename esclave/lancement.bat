681063d64aeea10032d07d3ce0b8f9df72417536
Hippolyte Mouthier <hippolyte.mouthier@gmail.com>
2015-04-23 15:04:30 +0200

    install windows

--- esclave/lancement.bat
+++ esclave/lancement.bat
@@ -1,3 +1,7 @@
-
+@echo off
 :--- Lancement de l'application  
-start /B ./node64/node ./esclave.js
+echo Historique de l'esclave de l'application MOJITO > esclave.log
+echo Date du jour de lancement: %date% >> esclave.log
+echo Heure de lancement : %time% >> esclave.log
+start /B ./soft/node64/node ./esclave.js >> esclave.log
+echo Esclave en cours ...
--- maitre/lancement.bat
+++ maitre/lancement.bat
@@ -1,9 +1,33 @@
+@echo off
 :----  Ajout des logiciels nécessaire dans le path
-set PATH=%PATH%;%CD%\exiftool
+set PATH=%PATH%;%CD%\soft\exiftool
 
 :--- Lancement de l'application  
+mkdir C:\data\db
+echo Historique du mongo de l'application MOJITO > mongo.log
+echo Date du jour de lancement: %date% >> mongo.log
+echo Heure de lancement : %time% >> mongo.log
+start /B ./soft/mongo/mongod >> mongo.log
+echo Mongo ...
+timeout 10 > NUL
 
-start /B ./soft/mongo/mongod
-start /B ./soft/node64/node ./utilisateur/bin/www
-start /B ./soft/node64/node ./metier/metier.js
-start /B ./soft/node64/node ./MMManager/MMManager.js
+echo Historique de l'utilisateur de l'application MOJITO > utilisateur.log
+echo Date du jour de lancement: %date% >> utilisateur.log
+echo Heure de lancement : %time% >> utilisateur.log
+start /B ./soft/node64/node ./utilisateur/bin/www >> utilisateur.log
+echo Utilisateur ...
+timeout 5 > NUL
+
+echo Historique du metier de l'application MOJITO > metier.log
+echo Date du jour de lancement: %date% >> metier.log
+echo Heure de lancement : %time% >> metier.log
+start /B ./soft/node64/node ./metier/metier.js >> metier.log
+echo Metier ...
+timeout 5 > NUL
+
+echo Historique du MMManager de l'application MOJITO > MMManager.log
+echo Date du jour de lancement: %date% >> MMManager.log
+echo Heure de lancement : %time% >> MMManager.log
+start /B ./soft/node64/node ./MMManager/MMManager.js >> MMManager.log
+echo MMManager ...
+echo Application en cours ...
