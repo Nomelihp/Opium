e5facce803585751b603ca4606d213bf3bf3d6dc
cdrouadaine <clement.drouadaine@orange.fr>
2015-04-24 13:19:48 +0200

    install & launch bash

--- esclave/lancement.sh
+++ esclave/lancement.sh
@@ -1,4 +1,4 @@
 echo "Historique de l'esclave de l'application MOJITO" > esclave.log
 echo "Heure du lancement : $(date)" >> esclave.log
-nohup ./softs_windows/node64/node ./esclave.js >> esclave.log &
+nohup echo ./softs_windows/node64/node ./esclave.js >> esclave.log &
 echo "Esclave lancé"
