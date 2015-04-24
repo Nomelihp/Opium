9be4e93489afb82775685efcb9eaa7d789ddf2f1
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2015-04-24 09:32:05 +0200

    intégraton du zippage dans le module métier

--- maitre/metier/metier.js
+++ maitre/metier/metier.js
@@ -5,6 +5,7 @@ var model = require('../model/mongo_config');
 var noyau_metier = require('./local_modules/noyau_metier');
 var js2xml = require('js2xmlparser');
 var config = require('../config.json');
+var zip = require('./local_modules/ResultZip');
 
 // Chemin vers le répertoire des données
 var repertoire_donnees = json.repertoire_donnees;
@@ -99,7 +100,10 @@ server.on('notification',function(message, data){
 
       });
 
-    }
+    } 
+    zip.zipFiles(config.repertoire_donnees+Besoin3.login+"/"+Besoin3._id,"calibration");
+    zip.zipFiles(config.repertoire_donnees+Besoin3.login+"/"+Besoin3._id,"orientation");
+    zip.zipFiles(config.repertoire_donnees+Besoin3.login+"/"+Besoin3._id,"liaison");
   }
 
   });
@@ -112,10 +116,11 @@ server.on('notification',function(message, data){
       Besoin2 = new model.besoins(besoin2[k]);
       fs.writeFile(config.repertoire_donnees+config.login+"/"+Besoin2._id+"/AperiCloud_MEP_selectionInfo.xml",js2xml("SelectionInfo",Besoin2.masque3D.SelectionInfo),function(err){
         if(err) console.log('[ERREUR: Erreur dans l\'enregistrement du fichier masque]');
-        console.log('enregistrement du fichier masque réussit !'+config.repertoire_donnees+'/'+config.login+'/'+Besoin2._id+'/AperiCloud_MEP_selectionInfo.xml');
+        console.log('enregistrement du fichier masque réussit !'+config.repertoire_donnees+config.login+'/'+Besoin2._id+'/AperiCloud_MEP_selectionInfo.xml');
       });
 
         noyau_metier.appariement_dense(Besoin2);
+        
         setTimeout(function(){
           client.request('notification', {boulot:"oui"}, function(data){
             console.log('[info: metier.js: Notification Envoyée a l u tilsiateur pour l appriement sense]');
